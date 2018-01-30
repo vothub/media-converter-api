58de873ad3ab60e9a8e9fc6210b8d8ff16e1cd52
Vot <vot@siliconfen.co>
2018-01-30 17:59:31 +0000

    Fixed status indicator, allowing gif output

--- routes/api/convert/get.js
+++ routes/api/convert/get.js
@@ -6,12 +6,14 @@ function handlerGet(req, res) {
   form += '<select name="converto-format">';
   form += '<option>mp4</option>';
   form += '<option>mp3</option>';
+  form += '<option>gif</option>';
   form += '<option>flv</option>';
   form += '<option>ogv</option>';
   form += '<option>ogg</option>';
   form += '</select><br />';
   form += '<input type="submit">';
   form += '</form>';
+  form += '<small>Max size: 50MB</small>';
   form += '</body></html>';
 
   res.send(form);
