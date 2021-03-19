ebca185cc8651c00f9fe16e42c54dacef5a801c9
Vot <vot@odro.co.uk>
2021-03-19 23:50:06 +0000

    Lint

--- routes/pages/jobs/list.js
+++ routes/pages/jobs/list.js
@@ -1,14 +1,9 @@
 const jobLib = require('../../../lib/job');
 
-// const _ = require('lodash');
-// const jobLib = require('../../lib/job');
-// const config = require('../../lib/config');
-
 function renderJobsPage(req, res) {
-  const collection = 'jobs';
-  jobLib.getAllJobs({}, (jobs) => {
+  return jobLib.getAllJobs({}, (jobs) => {
     if (jobs.error) {
-      return res.render('pages/error',  { error: 'Error while fetching jobs.' });
+      return res.render('pages/error', { error: 'Error while fetching jobs.' });
     }
     return res.render('pages/jobs/list', { jobs });
   });
